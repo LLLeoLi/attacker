6286191d769251d148dba8ed34faafcd7698d2d8
Hao <89140114+LLLeoLi@users.noreply.github.com>
2023-12-21 17:19:07 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // 常用库自动导入
+    AutoImport({
+      // import目标
+      include:[
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/, /\.vue\?vue/, // .vue
+      ],
+      // 自动导入的包
+      imports:['vue','vue-router','@vueuse/core'],
+      resolvers: [ElementPlusResolver()],
+    }),
+    // 组件自动导入
+    Components({
+      // 文件夹
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      // 支持深度导入
+      deep: true,
+      include: [/\.vue$/, /\.vue\?vue/],
+      exclude: [
+        /[\\/]node_modules[\\/]/,
+        /[\\/]\.git[\\/]/,
+        /[\\/]\.nuxt[\\/]/,
+      ],
+      resolvers: [ElementPlusResolver()],
+      directoryAsNamespace: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: ['.js',',ts','.json','.jsx','.tsx','.vue'],
+  }
+})
